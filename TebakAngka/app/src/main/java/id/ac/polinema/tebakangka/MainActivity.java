3554cf4d24f29a11f55de6d262f86d51278d0b7b
Angga Maulana Athaariq <am.athaariq@gmail.com>
2019-09-07 17:44:41 +0700

    Update Tebak Angka ( Nambah Reset )

--- TebakAngka/app/src/main/java/id/ac/polinema/tebakangka/MainActivity.java
+++ TebakAngka/app/src/main/java/id/ac/polinema/tebakangka/MainActivity.java
@@ -1,36 +1,82 @@
 package id.ac.polinema.tebakangka;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import java.util.Random;
 import android.widget.Toast;
 
-import java.util.Random;
+import org.w3c.dom.Text;
+
 
 public class MainActivity extends AppCompatActivity {
-
+	private Button rtReset;
+	private Button btnAngka;
+	private EditText etAngka;
+	private int angkaRandom = (int) (Math.random() * 3);
 	//  TODO: deklarasikan variabel di sini
+//	int angkaRandom = (int) (Math.random() * 0);
+//	int a = (int) (Math.random() * 10);
+//	int c = 2;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		btnAngka = (Button) findViewById(R.id.btTebakAngka);
+		etAngka = (EditText) findViewById(R.id.etTebak);
+		rtReset = (Button) findViewById(R.id.resButton);
+		angkaRandom = buatAngkaRandom(1, 3);
+//		initRandomNumber(buatAngkaRandom();
 		// TODO: bind layout di sini
 	}
 
 	// TODO: generate angka random di sini
 	private void initRandomNumber() {
+		btnAngka.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View view) {
+				if (!etAngka.equals("")) {
+					handleGuess(view);
+				}
+			}
+		});
 
 	}
-
 	public void handleGuess(View view) {
-		// TODO: Tambahkan logika untuk melakukan pengecekan angka
+		int angkaTebakan = Integer.parseInt(etAngka.getText().toString());
+		if (angkaTebakan == angkaRandom) {
+			btnAngka.setClickable(false);
+			Toast.makeText(this, "Tebakan Anda Benar", Toast.LENGTH_SHORT).show();
+		} else if (angkaTebakan < angkaRandom) {
+			Toast.makeText(this, "Tebakan Anda Terlalu KECIL", Toast.LENGTH_SHORT).show();
+		} else if (angkaTebakan > angkaRandom) {
+			Toast.makeText(this, "Tebakan Anda Terlalu BESAR", Toast.LENGTH_SHORT).show();
+		} else {
+			Toast.makeText(this, "Tebakan Anda Salah", Toast.LENGTH_SHORT).show();
+		}
+		angkaRandom = buatAngkaRandom(1, 3);
 	}
+	private int buatAngkaRandom(int angkaMin, int angkaMax) {
+		Random rand = new Random();
+		int randomNum = rand.nextInt(angkaMax - angkaMin) + angkaMin;
+		return randomNum;
+	}
+
+	// TODO: Tambahkan logika untuk melakukan pengecekan angka
 
 	public void handleReset(View view) {
 		// TODO: Reset tampilan
+		rtReset.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View view) {
+				initRandomNumber();
+			}
+		});
 	}
 }
